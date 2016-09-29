152490a12f90a4ba643b9a33cc2c3cd9c056dfe2
JooeunAhn <jflow@hanyang.ac.kr>
2016-09-30 03:10:36 +0900

    STATIC & MEDIA 설정

--- zochak/zochak/urls.py
+++ zochak/zochak/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from blog.views import index
 
@@ -22,3 +24,5 @@ urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^blog/', include('blog.urls', namespace="blog")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
